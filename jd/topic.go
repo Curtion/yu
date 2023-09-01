9185b7ddcee2ad1306a4b705b0b7385dfc603f7b
Curtion <curtion@126.com>
2023-09-01 15:04:46 +0800

    feat: 添加所有topic地址

--- jd/topic.go
+++ jd/topic.go
@@ -17,3 +17,13 @@ func NewTopic(productKey, deviceName string) *Topic {
 func (t *Topic) GetLatestDataTopic() string {
 	return fmt.Sprintf("iot/%s/%s/thing/event/property/pack/post", t.productKey, t.deviceName)
 }
+
+func (t *Topic) GetSubTopics() []string {
+	return []string{
+		fmt.Sprintf("iot/%s/#/thing/event/property/pack/post_reply", t.productKey),    // 最新属性、事件上报回复
+		fmt.Sprintf("iot/%s/#/thing/event/property/history/post_reply", t.productKey), // 历史属性、事件上报回复
+		fmt.Sprintf("iot/%s/#/thing/service/invoke", t.productKey),                    // 服务调用
+		fmt.Sprintf("iot/%s/#/sys/cmd/invoke", t.productKey),                          // 系统指令调用
+		fmt.Sprintf("iot/%s/#/thing/transport/down", t.productKey),                    // 下行透传
+	}
+}

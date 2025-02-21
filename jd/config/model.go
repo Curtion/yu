9d5aff5001f1e2ec5be1f225dd626bb2960496fb
Curtion <curtion@126.com>
2025-02-21 16:32:22 +0800

    feat: 新增ConfigSetCall和ConfigCheckCall结构体及相关参数定义

--- jd/config/model.go
+++ jd/config/model.go
@@ -1,18 +1,26 @@
 package config
 
-type Identity struct {
-	Pk   string `json:"pk"`
-	Name string `json:"name"`
+type ConfigSetCall struct {
+	MsgId   string          `json:"msgId"`
+	Version string          `json:"version"`
+	Method  string          `json:"method"`
+	Params  configSetParams `json:"params"`
 }
 
-type ConfigCall struct {
-	MsgId   string `json:"msgId"`
-	Version string `json:"version"`
-	Method  string `json:"method"`
-	Params  params `json:"params"`
+type configSetParams struct {
+	Version    string                 `json:"version"`
+	Config     map[string]interface{} `json:"config"`
+	SubDevices []SubDevice            `json:"subDevices"`
 }
 
-type params struct {
+type ConfigCheckCall struct {
+	MsgId   string            `json:"msgId"`
+	Version string            `json:"version"`
+	Method  string            `json:"method"`
+	Params  configCheckParams `json:"params"`
+}
+
+type configCheckParams struct {
 	Self       string            `json:"self"`
 	SubDevices []ParamsSubDevice `json:"subDevices"`
 }
@@ -26,6 +34,11 @@ type SubDevice struct {
 	Identity Identity               `json:"identity"`
 	Version  string                 `json:"version"`
 	Config   map[string]interface{} `json:"config"`
+}
+
+type Identity struct {
+	Pk   string `json:"pk"`
+	Name string `json:"name"`
 }
 
 type data struct {

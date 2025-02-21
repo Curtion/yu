8525631f4e5b99c5459054e70d0e89de9519e63d
Curtion <curtion@126.com>
2025-02-21 16:11:42 +0800

    feat: 新增ConfigCall结构体及相关参数定义

--- jd/config/model.go
+++ jd/config/model.go
@@ -5,6 +5,23 @@ type Identity struct {
 	Name string `json:"name"`
 }
 
+type ConfigCall struct {
+	MsgId   string `json:"msgId"`
+	Version string `json:"version"`
+	Method  string `json:"method"`
+	Params  params `json:"params"`
+}
+
+type params struct {
+	Self       string            `json:"self"`
+	SubDevices []ParamsSubDevice `json:"subDevices"`
+}
+
+type ParamsSubDevice struct {
+	Pk   string `json:"pk"`
+	Name string `json:"name"`
+}
+
 type SubDevice struct {
 	Identity Identity               `json:"identity"`
 	Version  string                 `json:"version"`
